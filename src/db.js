8aed5e9fc67039f14b26d0d732d6265af5159456
Ashraf <ashrafhussain2265@gmail.com>
2025-12-15 03:26:50 +0530

    fix3

--- src/db.js
+++ src/db.js
@@ -9,9 +9,8 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false,
+    ca: process.env.DB_CA_CERT,
   },
 });
 
